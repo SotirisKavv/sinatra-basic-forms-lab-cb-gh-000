0bcf97ee4ffc00d24a8646452eb045cfd7294812
Sotiris Kavvouras <sotos.kavvouras@gmail.com>
2019-09-24 15:00:27 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -3,10 +3,20 @@ class Puppy
     attr_accessor :name, :months_old
     attr_reader :breed
 
+    @@all = []
+    
     def initialize(name = nil, breed = nil, months_old = nil)
       @name = name
       @breed = breed
       @months_old = months_old
+    end
+
+    def save
+      self.all << self
+    end
+
+    def self.all
+      @@all
     end
 
     end
