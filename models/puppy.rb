db10af09d527b9f413d0347fbe16b278f8a23ad3
Sotiris Kavvouras <sotos.kavvouras@gmail.com>
2019-09-24 15:16:40 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -5,7 +5,7 @@ class Puppy
 
     @@all = []
 
-    def initialize(name = nil, breed = nil, months_old = nil)
+    def initialize(name, breed, months_old)
       @name = name
       @breed = breed
       @months_old = months_old
